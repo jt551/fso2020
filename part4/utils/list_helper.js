d00b132f8b23b99974a2feaff4faba44ef4925de
jt551 <jtwebapps@gmail.com>
2021-01-12 17:57:54 +0200

    4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -11,7 +11,14 @@ const totalLikes = (blogList) => {
     : blogList.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogList) => {
+  return blogList.length === 0 ? {} : blogList.reduce((max, blog) => blog.likes > max.likes ? blog : max)
+  //if(blogList.length === 0) return {}
+  //return blogList.reduce((max, blog) => blog.likes > max.likes ? blog : max)
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog
 }
