301eea54215e56ed7a5d167211fa63818088bd05
jt551 <jtwebapps@gmail.com>
2021-01-12 12:16:08 +0200

    4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogList) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+  return blogList.length === 0
+    ? 0
+    : blogList.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
