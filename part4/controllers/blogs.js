62dc7f35ad5d498f3a751f1cadeda0c89a45e780
jt551 <jtwebapps@gmail.com>
2021-01-21 12:07:01 +0200

    4.19

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,8 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { name: 1, username: 1 })
@@ -9,16 +11,23 @@ blogRouter.get('/', async (request, response) => {
 
 blogRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(body.userId)
-  if (!user) return response.status(400).send('incorrect user information')
-  if (!body.likes) body.likes = 0
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, config.SECRET)
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  //if (!user) return response.status(400).send('incorrect user information')
+  //if (!body.likes) body.likes = 0
   if (!body.title && !body.url)
     return response.status(400).send('title & url missing')
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes,
+    likes: body.likes || 0,
     user: user._id,
   })
   const savedBlog = await blog.save()
@@ -44,5 +53,14 @@ blogRouter.put('/:id', async (request, response) => {
   })
   response.json(updatedBlog)
 })
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 module.exports = blogRouter
