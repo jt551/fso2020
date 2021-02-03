a169e39063aad1ce02ea16b92816303f628dea84
jt551 <jtwebapps@gmail.com>
2021-02-03 22:59:20 +0200

    5.2

--- part5/src/App.js
+++ part5/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [errorMessage, setErrorMessage] = useState('')
@@ -13,6 +14,14 @@ const App = () => {
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      // SET TOKEN
+    }
+  }, [])
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -21,6 +30,10 @@ const App = () => {
         username,
         password,
       })
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+      // SET TOKEN
       setUser(user)
       setUsername('')
       setPassword('')
@@ -30,6 +43,10 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+  const logoutButtonHandler = () => {
+    window.localStorage.removeItem('loggedBlogAppUser')
+    setUser('')
   }
   
   const loginForm = () => (
@@ -69,12 +86,12 @@ const App = () => {
   return (
     <div>
       <h1>Blog app</h1>
-
+      <Notification message={errorMessage} />
       {user === null ? (
         loginForm()
       ) : (
         <div>          
-          <p>{user.name} logged in</p>
+          <p>{user.name} logged in</p><button onClick={logoutButtonHandler}/>
           {blogList()}
         </div>
       )}
