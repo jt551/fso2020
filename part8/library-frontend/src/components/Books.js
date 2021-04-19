80a6610dc3ac8bb0bb5cfb7b4263c9cdb1bf3104
jt551 <jtwebapps@gmail.com>
2021-04-19 16:29:05 +0300

    8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,12 +1,13 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import { ALL_BOOKS } from '../queries'
 import { useQuery } from '@apollo/client'
+import Select from 'react-select'
 
 const Books = (props) => {
 
   const books = useQuery(ALL_BOOKS)
-
+  const [selectedGenre, setSelectedGenre] = useState('')
   if (!props.show) {
     return null
   }
@@ -21,11 +22,33 @@ const Books = (props) => {
       </div>
     )
   }
-
+  const genreSet = new Set()
+  genreSet.add('')
+  books.data.allBooks.map(g => g.genres.forEach(element => {
+    genreSet.add(element)
+  }))
+  console.log(genreSet);
+  const selectHandler = (value) => {
+    //console.log(value)
+    setSelectedGenre(value.value)
+  }
+  const GenreSetAsObj = Array.from(genreSet).map( genre => (    
+    {
+      value: genre,
+       label: genre
+    }
+  ))
+  console.log(selectedGenre)
+  let filteredBooks = books.data.allBooks
+  if(selectedGenre){
+    filteredBooks = books.data.allBooks.filter(b => b.genres.includes(selectedGenre))
+  }  
+  console.log(filteredBooks);
   return (
     <div>
-      <h2>books</h2>
-
+      <h1>BOOKS</h1>
+      <h4>Filter by genre</h4>
+      <Select value={selectedGenre} onChange={selectHandler} options={GenreSetAsObj} />
       <table>
         <tbody>
           <tr>
@@ -37,7 +60,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.data.allBooks.map(a =>
+          {filteredBooks.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
