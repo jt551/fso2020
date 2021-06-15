17486f9e840a941e33018be0abb01a4400d80415
jt551 <jtwebapps@gmail.com>
2021-06-15 13:53:04 +0300

    9.5

--- part9/ts1/bmiCalculator.ts
+++ part9/ts1/bmiCalculator.ts
@@ -1,44 +1,53 @@
+export const calculateBmi = (a: number, b: number): string => {
+  if (!(a > 0) || !(b > 0)) {
+    throw new Error('invalid input');
+  }
 
-const calculateBmi = (a: number, b: number): String => {
-    if (!(a > 0) || !(b > 0)) {
-        throw new Error('invalid input');
-    }
+  const height = a / 100;
+  const weight = b;
 
-    const height = a / 100;
-    const weight = b;
+  const bmi = weight / (height * height);
 
-    const bmi = weight / (height * height);
+  if (bmi < 18.5) {
+    return 'Low, (weight is below normal)';
+  }
+  if (18.5 <= bmi && bmi <= 25) {
+    return 'Normal, (healthy weight)';
+  }
+  return 'High, (weight higher than normal)';
+};
 
-    if (bmi < 18.5) {
-        return "Low, (weight is below normal)";
-    }
-    if (18.5 <= bmi && bmi <= 25) {
-        return "Normal, (healthy weight)";
-    }
-    return "High, (weight higher than normal)";
+export const parseArguments = (args: Array<string>): Array<number> => {
+  if (args.length < 2) throw new Error('Not enough arguments');
+  if (args.length > 2) throw new Error('Too many arguments');
 
+  if (!isNaN(Number(args[0])) && !isNaN(Number(args[1]))) {
+    const parsedArray = [Number(args[0]), Number(args[1])];
+    return parsedArray;
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
+export const checkAndReturnBmi = (h: any, w: any): string => {
+
+    const height : number = Number(h)
+    const weight : number = Number(w)
+    let bmiReturn: string = 'malformatted parameters'
+
+    if(!isNaN(height) && !isNaN(weight)) {
+        bmiReturn = calculateBmi(height, weight);
+    } 
+
+    return bmiReturn;
 }
-
-const parseArguments = (args: Array<string>): Array<number> => {
-    if (args.length < 2) throw new Error('Not enough arguments');
-    if (args.length > 2) throw new Error('Too many arguments');
-
-    if (!isNaN(Number(args[0])) && !isNaN(Number(args[1]))) {
-        const parsedArray = [Number(args[0]), Number(args[1])]
-        return parsedArray
-    } else {
-        throw new Error('Provided values were not numbers!');
-    }
-}
-
+/*
 const userArgs = process.argv.slice(2);
 
 try {
-    const [userHeight, userWeight] = parseArguments(userArgs)
-    console.log(calculateBmi(userHeight, userWeight))
+  const [userHeight, userWeight] = parseArguments(userArgs);
+  console.log(calculateBmi(userHeight, userWeight));
 } catch (error) {
-    console.log('Error : ', error.message);
+  console.log('Error : ', error.message);
 }
-
-
-
+*/
--- part9/ts1/index.ts
+++ part9/ts1/index.ts
@@ -1,13 +1,35 @@
 import express from 'express';
+import { checkAndReturnBmi } from './bmiCalculator';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
-    res.send('Hello Full Stack!');
+  res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const { height, weight }: any = req.query;
+  //console.log(height, weight);
+
+  const bmi: string = checkAndReturnBmi(height, weight);
+
+  if (bmi === 'malformatted parameters') {
+    res.send({
+      error: 'malformatted parameters',
+    });
+    return;
+  }
+
+  res.send({
+    weight,
+    height,
+    bmi,
+  });
 });
 
 const PORT = 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);    
+  console.log(`Server running on port ${PORT}`);
 });
