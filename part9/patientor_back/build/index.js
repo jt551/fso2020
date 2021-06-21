0e04e7ebff18419427fc611eec21dd8a8ab8df17
jt551 <jtwebapps@gmail.com>
2021-06-21 17:44:05 +0300

    9.9

--- part9/patientor_back/build/index.js
+++ part9/patientor_back/build/index.js
@@ -4,10 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
+const allowedOrigins = ['http://localhost:3000'];
+const options = {
+    origin: allowedOrigins
+};
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors_1.default(options));
 app.use(express_1.default.json());
-const PORT = 3000;
-app.get('/ping', (_req, res) => {
+const PORT = 3001;
+app.get('/api/ping', (_req, res) => {
+    console.log('pong_');
     res.send('pong');
 });
 app.listen(PORT, () => {
--- part9/patientor_back/index.ts
+++ part9/patientor_back/index.ts
@@ -1,10 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
+
+const allowedOrigins = ['http://localhost:3000'];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors(options));
 app.use(express.json());
 
-const PORT = 3000;
 
-app.get('/ping', (_req, res) => {
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+  console.log('pong_');
+  
   res.send('pong');
 });
 
