b138c3bbd3dc8ee45c7c7534abf8aabc25851366
jt551 <jtwebapps@gmail.com>
2021-07-03 18:27:27 +0300

    9.12

--- part9/patientor_back/src/routes/patientsRouter.ts
+++ part9/patientor_back/src/routes/patientsRouter.ts
@@ -1,18 +1,35 @@
-import { getFilteredPatientEntries } from './../services/patientService';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { getFilteredPatientEntries, addNewPatient } from './../services/patientService';
 import express from 'express';
+import { FilteredPatientDetails } from '../types';
 
 const router = express.Router();
 
-const filteredPatients = getFilteredPatientEntries();
+const getPatients = (): FilteredPatientDetails [] => {
+  const updatedPatients: FilteredPatientDetails [] = getFilteredPatientEntries();
+  return updatedPatients;
+};
 
 router.get('/', (_req, res) => {
   console.log('/api/patients get request');
-  res.send(filteredPatients);
+  const filteredPatientList: FilteredPatientDetails [] = getPatients();
+  res.send(filteredPatientList);
 });
 
-router.post('/', (_req, res) => {
-    console.log('/api/patients post request');  
-    res.send('not implemented');
+router.post('/', (req, res) => {
+    console.log('/api/patients post request');
+    
+    const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+   
+    const newPatientEntry = addNewPatient({
+      name,
+      dateOfBirth,
+      ssn,
+      gender,
+      occupation
+    });
+
+    res.send(newPatientEntry);
   });
 
 export default router;
--- part9/patientor_back/src/services/patientService.ts
+++ part9/patientor_back/src/services/patientService.ts
@@ -1,14 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {v4 as uuid} from 'uuid';
 import patientData from '../data/patients.json';
-import { Patient, FilteredPatientDetails } from '../types';
+import { Patient, NewPatient, FilteredPatientDetails } from '../types';
 
 const patients: Array<Patient> = patientData;
 
 export const getFilteredPatientEntries = (): FilteredPatientDetails [] => {
-    return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    const filteredPatientList: FilteredPatientDetails [] = patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
         id,
         name,
         dateOfBirth,
         gender,
         occupation
     }));
+
+    return filteredPatientList;
+};
+
+export const addNewPatient = (entry: NewPatient) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    const id: string = uuid();
+    const newPatientEntry: Patient = {
+        id,
+        ...entry
+    };
+
+    patients.push(newPatientEntry);
+    return newPatientEntry;
 };
